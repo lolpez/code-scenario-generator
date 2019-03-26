bc2c2abb2da1e37cdbb619248ea90b320c8c250d
Daniel Lopez <lopezsandi1666@gmail.com>
2019-03-26 11:52:33 -0400

    Added UI functionality

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,34 @@
     const socket = io("/socket-index");
     const codeScenarioElement = document.getElementById("code-scenario");
     const codeResultElement = document.getElementById("code-result");
+    const pasteButtonElement = document.getElementById("paste-button");
+    const copyButtonElement = document.getElementById("copy-button");
 
     codeScenarioElement.addEventListener("keyup", () => {
-        socket.emit("new-scenario", codeScenarioElement.value);
+        sendData();
     });
 
     socket.on("scenario-generated", (response) => {
         codeResultElement.value = response.result;
     });
+
+    pasteButtonElement.addEventListener("click", () => {
+        navigator.clipboard.readText()
+            .then((text) => {
+                codeScenarioElement.value = text;
+                sendData();
+            })
+            .catch((err) => {
+                alert("Failed to read clipboard contents");
+            });
+    });
+
+    copyButtonElement.addEventListener("click", () => {
+        codeResultElement.select();
+        document.execCommand("copy");
+    });
+
+    function sendData() {
+        socket.emit("new-scenario", codeScenarioElement.value);
+    }
 })();
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-
-
 const indexRouter = (io) => {
 	/* GET home page. */
 	router.get("/", function (req, res, next) {
