1e58523d8f4602e7950cbc03546d2b43e41bb6dc
Daniel Lopez <lopezsandi1666@gmail.com>
2019-04-11 16:21:33 -0400

    Added advance configuration for step formats

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,16 @@
 (function () {
+    const defaultRule = "{// }{counter}{. }{step}{br}{br}";
     const codeScenarioElement = document.getElementById("code-scenario");
     const codeResultElement = document.getElementById("code-result");
     const pasteButtonElement = document.getElementById("paste-button");
     const copyButtonElement = document.getElementById("copy-button");
     const stepsCountElement = document.getElementById("steps-count");
     const loaderElement = document.getElementById("loader");
+    const configurationElement = document.getElementById("configuration");
+    const ruleElement = document.getElementById("rule");
+    const ruleResetElement = document.getElementById("reset-rule");
+
+    ruleElement.value = defaultRule;
 
     codeScenarioElement.addEventListener("keyup", () => {
         convertScenario(codeScenarioElement.value);
@@ -26,6 +32,20 @@
         document.execCommand("copy");
     });
 
+    configurationElement.addEventListener("click", () => {
+        configurationElement.classList.toggle("active");
+        var content = configurationElement.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+
+    ruleResetElement.addEventListener("click", () => {
+        ruleElement.value = defaultRule;
+    });
+
     function updateInterfaceResult(convertedScenario) {
         codeResultElement.value = convertedScenario.result;
         if (convertedScenario.stepsCount > 0) {
@@ -36,6 +56,23 @@
         loaderElement.style.display = "none";
     }
 
+    function getRule() {
+        var rules = [];
+        for (var i = 0; i < ruleElement.value.length; i++) {
+            if (ruleElement.value.charAt(i) == "{") {
+                var rule = "";
+                for (var j = i + 1; j < ruleElement.value.length; j++) {
+                    if (ruleElement.value.charAt(j) == "}") {
+                        break;
+                    }
+                    rule += ruleElement.value.charAt(j);
+                }
+                rules.push(rule);
+            }
+        }
+        return rules;
+    }
+
     function convertScenario(scenario) {
         loaderElement.style.display = "inline-block";
         if (scenario.length > 0) {
@@ -44,7 +81,21 @@
             var stepsCount = 0;
             steps.forEach((step) => {
                 stepsCount++;
-                result += `// ${stepsCount}. ${step}\n\n`;
+                getRule().forEach((rule) => {
+                    switch (rule) {
+                        case "counter":
+                            result += stepsCount;
+                            break;
+                        case "step":
+                            result += step;
+                            break;
+                        case "br":
+                            result += "\n";
+                            break;
+                        default:
+                            result += rule;
+                    }
+                });
             });
             updateInterfaceResult({
                 stepsCount: stepsCount,
@@ -59,11 +110,11 @@
     }
 
     // Service worker
-    if ("serviceWorker" in navigator) {
+    /*if ("serviceWorker" in navigator) {
         navigator.serviceWorker.register("sw.js").then((reg) => {
             console.log("Service Worker Registered 🤩", reg.scope);
         }).catch(function (err) {
             console.log("Service Worker Failed to Register 😩", err);
         });
-    }
+    }*/
 })();
